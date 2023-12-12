278c22a9f0d03bbfe880c3a2a32add663b7ade3f
Leonardo Risco <leo99153@gmail.com>
2023-12-12 16:17:35 -0500

    Material UI dependencies and routerDom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme = {theme}>
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </BrowserRouter>
+  </ThemeProvider>  
 )
