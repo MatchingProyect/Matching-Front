d96c60e134a746d459214e6f148863fa57000144
Leonardo Risco <leo99153@gmail.com>
2023-12-13 20:01:47 -0500

    module.css

--- src/components/friendsContainer/friendsContainer.jsx
+++ src/components/friendsContainer/friendsContainer.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import styles from './friendsContainer.styles.css';
+import styles from './friendsContainer.module.css';
 import { Link } from 'react-router-dom';
 
 export default function FriendsContainer(){
