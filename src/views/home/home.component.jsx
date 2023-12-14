3105d9edc15127fe428be609551fea16982cdd26
Aquiles Popovich <popovichaquiles@gmail.com>
2023-12-14 00:07:48 -0300

    informacion traiga por la app de los estados globales (endpoints) y pasada a la home

--- src/views/home/home.component.jsx
+++ src/views/home/home.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './home.module.css';
 
-export default function Home() {
+export default function Home({sports, clubs, locations, users}) {
     return(
         <div>
             <h1>Hello from Home</h1>
