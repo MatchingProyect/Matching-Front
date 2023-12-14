29c219b4b9d04fb82727ed23827295b535a21c16
Aquiles Popovich <popovichaquiles@gmail.com>
2023-12-13 23:50:28 -0300

    redux terminado

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,29 +1,63 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    name: '',
-    username: '',
-    email: '',
-
-
+    allUsers: [],
+    allSports: [],
+    allClubs: [],
+    allLocations: []
 };
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers:{
-        addUser: (state, action) => {
-            const { name, username, email } = action.payload;
-            state.name = name;
-            state.username = username;
-            state.email = email;
-        },
-        changeEmail: (state, action) => {
-            const { email } = action.payload;
-            state.email = email;
-        },
+       setUsers: (state, action) =>{
+        state.allUsers = action.payload
+       },
+       setSports: (state, action)=>{
+        state.allSports = action.payload
+       },
+       setClubs: (state, action)=>{
+        state.allClubs = action.payload
+       },
+       setLocations: (state, action)=>{
+        state.allLocations = action.payload
+       }
     }
 })
 
-export const { addUser, changeEmail } = userSlice.actions;
+export const fetchUsers = ()=>async(dispatch)=>{
+    try {
+        const {data} = await axios('http://localhost:3000/users')
+        if(data.status) dispatch(setUsers(data.allUsers))
+    } catch (error) {
+        throw error.message
+    }
+}
+export const fetchSports = ()=>async(dispatch)=>{
+    try {
+        const {data} = await axios('http://localhost:3000/sports')
+        if(data.status) dispatch(setSports(data.allSports))
+    } catch (error) {
+        throw error.message
+    }
+}
+export const fetchLocations = ()=>async(dispatch)=>{
+    try {
+        const {data} = await axios('http://localhost:3000/locations')
+        if(data.status) dispatch(setLocations(data.allLocations))
+    } catch (error) {
+        throw error.message
+    }
+}
+export const fetchClubs = ()=>async(dispatch)=>{
+    try {
+        const {data} = await axios('http://localhost:3000/clubs')
+        if(data.status) dispatch(setClubs(data.allClubs))
+    } catch (error) {
+        throw error.message
+    }
+}
+
+export const { setClubs, setLocations, setUsers, setSports } = userSlice.actions;
 export default userSlice.reducer;
