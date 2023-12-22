a260e88bc6983ad6f0e623a4622c0a1e9d0e703c
Lucas Mella <lucks191@gmail.com"  git config --global user.name "LMellaVillanueva>
2023-12-22 19:28:03 -0300

    css de login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -17,20 +17,23 @@ const Login = () => {
   };
 
   return (
-    <>
+    <div className={styles.allContainer}>
       <form
         className={styles.formContainer}
         onSubmit={handleSubmit((data) => console.log(data))}
       >
-        <img
-          src="https://res.cloudinary.com/dbffmtz0y/image/upload/v1702491179/Matching_rlj4xk.svg"
-          alt="Logo"
-          className={styles.logo}
-        />
+        <div className={styles.logoContainer}>
+          <img
+            src="https://res.cloudinary.com/dbffmtz0y/image/upload/v1702491179/Matching_rlj4xk.svg"
+            alt="Logo"
+            className={styles.logo}
+          />
+        </div>
+
 
         <div className={styles.inputContainer}>
-          <div className={ styles.containerTitleLogin }>
-            <h2 className={ styles.titleLogin } >INICIO DE SESION</h2>
+          <div className={styles.containerTitleLogin}>
+            <h2 className={styles.titleLogin} >INICIO DE SESION</h2>
           </div>
           <div className={styles.contentController}>
             <label className={`${styles.labels} ${styles.emailLabel}`}>
@@ -62,65 +65,65 @@ const Login = () => {
 
           <div className={styles.contentController}>
             <label className={styles.labelsPass}>Contraseña:</label>
-              <Controller
-                name="password"
-                control={control}
-                rules={{
-                  required: "Este campo es requerido",
-                  minLength: {
-                    value: 8,
-                    message: "La contraseña debe tener al menos 8 caracteres",
-                  },
-                  maxLength: {
-                    value: 15,
-                    message: "La contraseña no debe exceder los 15 caracteres",
-                  },
-                }}
-                render={({ field }) => (
-                  <>
-                    <input
-                      className={styles.inputPass}
-                      type={showPassword ? "text" : "password"}
-                      {...field}
-                    />
-                  </>
-                )}
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                required: "Este campo es requerido",
+                minLength: {
+                  value: 8,
+                  message: "La contraseña debe tener al menos 8 caracteres",
+                },
+                maxLength: {
+                  value: 15,
+                  message: "La contraseña no debe exceder los 15 caracteres",
+                },
+              }}
+              render={({ field }) => (
+                <>
+                  <input
+                    className={styles.inputPass}
+                    type={showPassword ? "text" : "password"}
+                    {...field}
+                  />
+                </>
+              )}
+            />
+            <button
+              type="button"
+              className={styles.eyeButton}
+              onClick={togglePasswordVisibility}
+            >
+              <img
+                src="https://res.cloudinary.com/dbffmtz0y/image/upload/v1702489357/eye.slash_vthsb6.svg"
+                alt="Mostrar/Ocultar contraseña"
+                className={styles.eyeIcon}
               />
-              <button
-                type="button"
-                className={styles.eyeButton}
-                onClick={togglePasswordVisibility}
-              >
-                <img
-                  src="https://res.cloudinary.com/dbffmtz0y/image/upload/v1702489357/eye.slash_vthsb6.svg"
-                  alt="Mostrar/Ocultar contraseña"
-                  className={styles.eyeIcon}
-                />
-              </button>
-              {errors.password && <p className={ styles.pPass }>{errors.password.message}</p>}
-            </div>
+            </button>
+            {errors.password && <p className={styles.pPass}>{errors.password.message}</p>}
+          </div>
         </div>
 
         <button type="submit" className={styles.submitButton}>
           INICIAR SESION
         </button>
 
-        <Button sx={ { ..._styled.signWithGoogle} }>INICIAR SESION CON GOOGLE</Button>
+        <Button sx={{ ..._styled.signWithGoogle }}>INICIAR SESION CON GOOGLE</Button>
 
         <div className={styles.container}>
-  <p className={styles.registerText}>
-    ¿No tienes cuenta? <br />
-    <Link to="/registro" className={styles.registerLink}>
-      Regístrate
-    </Link>
-  </p>
+          <p className={styles.registerText}>
+            ¿No tienes cuenta? <br />
+            <Link to="/registro" className={styles.registerLink}>
+              Regístrate
+            </Link>
+          </p>
 
-  <Link to='/restaurar-contrasenia' className={styles.forgotPasswordText}>
-    ¿Olvidaste tu contraseña?
-  </Link>
-</div>
+          <Link to='/restaurar-contrasenia' className={styles.forgotPasswordText}>
+            ¿Olvidaste tu contraseña?
+          </Link>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
