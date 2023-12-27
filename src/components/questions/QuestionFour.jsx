bf7058881a98b2931936de38d5c05dcc85afb647
JMRodriguezV98 <mriciosama@gmail.com>
2023-12-27 13:59:20 -0500

    dato guardado de user en el useContext, todas las preguntas menos perfil deportivo

--- src/components/questions/QuestionFour.jsx
+++ src/components/questions/QuestionFour.jsx
@@ -10,7 +10,11 @@ export const QuestionFour = () => {
   const { datosUser,setDatosUser } = useUserContext();
 
   useEffect(() => {
-    console.log( datosUser )
+    setDatosUser({
+      ...datosUser,
+      sport: 'padel',
+      questionsAnsker: true,
+    })
   }, [])
   
 
@@ -20,14 +24,15 @@ export const QuestionFour = () => {
           <p className={ styles.titleQuestion } >¿Qué deporte jugas?</p>
           <p className={ styles.subtitleQuestion } >Puedes elegir mas de un deporte.</p>
           <div className={ styles.contentSports }>
-            <CardSport iconSport={ `${ urlIcons }/v1702540814/emojione-monotone_tennis_almief.svg` } nameSport={ 'Pádel' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541735/tenis_cyo2ka.svg` } nameSport={ 'Tenis' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541734/futbol_isrkf2.svg` } nameSport={ 'Football' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541734/golf_t9uvge.svg` } nameSport={ 'Golf' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541734/basquet_bqbqrl.svg` } nameSport={ 'Basquet' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541734/hokey_upskgq.svg` } nameSport={ 'Hockey' }/>
-            <CardSport iconSport={ `${ urlIcons }/v1702541735/squash_hjiwwz.svg` } nameSport={ 'Squash' }/>
+            <CardSport iconSport={ `${ urlIcons }/v1702540814/emojione-monotone_tennis_almief.svg` } nameSport={ 'Pádel' } state={ true } />
+            <CardSport iconSport={ `${ urlIcons }/v1702541735/tenis_cyo2ka.svg` } nameSport={ 'Tenis' } state={ false }/>
+            <CardSport iconSport={ `${ urlIcons }/v1702541734/futbol_isrkf2.svg` } nameSport={ 'Football' } state={ false } />
+            <CardSport iconSport={ `${ urlIcons }/v1702541734/golf_t9uvge.svg` } nameSport={ 'Golf' } state={ false } />
+            <CardSport iconSport={ `${ urlIcons }/v1702541734/basquet_bqbqrl.svg` } nameSport={ 'Basquet' } state={ false } />
+            <CardSport iconSport={ `${ urlIcons }/v1702541734/hokey_upskgq.svg` } nameSport={ 'Hockey' } state={ false } />
+            <CardSport iconSport={ `${ urlIcons }/v1702541735/squash_hjiwwz.svg` } nameSport={ 'Squash' } state={ false } />
           </div>
+          <span className={ styles.infoSports } >Pronto más deportes disponibles.</span>
       </div>
     </>
   )
