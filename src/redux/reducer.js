2d2e96d51e8c40f57331fa7a885a8b41fd4be518
Lucas Mella <lucks191@gmail.com"  git config --global user.name "LMellaVillanueva>
2023-12-30 12:25:32 -0300

    Funcionalidad SearchBar incompleta, agregar estado global amigos

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,7 +5,8 @@ const initialState = {
     allUsers: [],
     allSports: [],
     allClubs: [],
-    allCourts: []
+    allCourts: [],
+    allFriends: []
 };
 
 export const userSlice = createSlice({
@@ -26,14 +27,17 @@ export const userSlice = createSlice({
        },
        setProfiles: (state, action) =>{
         state.allProfiles = action.payload
+       },
+       setFriends: (state, action) => {
+        state.allFriends = action.payload;
        }
     }
 })
 
 export const fetchProfiles = ()=>async(dispatch)=>{
     try {
-        //  const {data} = await axios('/profiles')
-        // if(data.status) dispatch(setProfiles(data.allProfiles))
+         const {data} = await axios('/profiles')
+        if(data.status) dispatch(setProfiles(data.allProfiles))
     } catch (error) {
         throw error.message
     }
@@ -41,36 +45,46 @@ export const fetchProfiles = ()=>async(dispatch)=>{
 
 export const fetchUsers = ()=>async(dispatch)=>{
     try {
-        //  const {data} = await axios('/users')
-        // if(data.status) dispatch(setUsers(data.allUsers))
+         const {data} = await axios('/users')
+        if(data.status) dispatch(setUsers(data.allUsers))
     } catch (error) {
         throw error.message
     }
 }
+
+export const fetchFriends = () => async(dispatch) => {
+    try {
+        const {data} = await axios('/users')
+       if(data.status) dispatch(setFriends(data.allUsers))
+   } catch (error) {
+       throw error.message
+   }
+}
+
 export const fetchSports = ()=>async(dispatch)=>{
     try {
-        // const {data} = await axios('/sports')
-        // if(data.status) dispatch(setSports(data.allSports))
+        const {data} = await axios('/sports')
+        if(data.status) dispatch(setSports(data.allSports))
     } catch (error) {
         throw error.message
     }
 }
 export const fetchCourts = ()=>async(dispatch)=>{
     try {
-        // const {data} = await axios('/courts')
-        // if(data.status) dispatch(setCourts(data.allCourts))
+        const {data} = await axios('/courts')
+        if(data.status) dispatch(setCourts(data.allCourts))
     } catch (error) {
         throw error.message
     }
 }
 export const fetchClubs = ()=>async(dispatch)=>{
     try {
-        //  const {data} = await axios('/clubs')
-        //  if(data.status) dispatch(setClubs(data.allClubs))
+         const {data} = await axios('/clubs')
+         if(data.status) dispatch(setClubs(data.allClubs))
     } catch (error) {
         throw error.message
     }
 }
 
-export const { setClubs, setCourts, setUsers, setSports, setProfiles } = userSlice.actions;
+export const { setClubs, setCourts, setUsers, setSports, setProfiles, setFriends } = userSlice.actions;
 export default userSlice.reducer;
--- src/views/cardUsers/CardUser.jsx
+++ src/views/cardUsers/CardUser.jsx
@@ -7,6 +7,7 @@ const CardUser = ({user}) => {
       <Link to={`/user/${user.id}`}>
       <h2>{user.name}</h2>
       </Link>
+      <button>Agregar amigo</button>
     </div>
   )
 }
