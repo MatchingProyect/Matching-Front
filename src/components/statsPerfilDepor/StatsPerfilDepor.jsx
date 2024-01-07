16e1750bd366bb2c7d3afcb0a15ee706ab775901
diegogonr <diegog@explorandes.com>
2024-01-07 17:02:18 -0500

    profile

--- src/components/statsPerfilDepor/StatsPerfilDepor.jsx
+++ src/components/statsPerfilDepor/StatsPerfilDepor.jsx
@@ -6,14 +6,14 @@ export default function StatsPerfilDepor(props){
     return(
         <div className = {styles.statsPerfilDeportDiv}>
             <div className = {styles.divRow}>
-                <p className = {styles.text}>Ganadas: {stats.ganadas}</p>
-                <p className = {styles.text}>Perdidas: {stats.perdidas}</p>
-                <p className = {styles.text}>Jugadas: {stats.jugadas}</p>
+                <p className = {styles.text}>Ganadas: {stats?.ganadas}</p>
+                <p className = {styles.text}>Perdidas: {stats?.perdidas}</p>
+                <p className = {styles.text}>Jugadas: {stats?.jugadas}</p>
             </div>
             <div className = {styles.divRow}>
-            <p className = {styles.text}>Asistencias: {stats.asistencias}</p>
-                <p className = {styles.text}>Canceladas: {stats.canceladas}</p>
-                <p className = {styles.text}>Organizadas: {stats.organizadas}</p>
+            <p className = {styles.text}>Asistencias: {stats?.asistencias}</p>
+                <p className = {styles.text}>Canceladas: {stats?.canceladas}</p>
+                <p className = {styles.text}>Organizadas: {stats?.organizadas}</p>
             </div>            
         </div>
     );
