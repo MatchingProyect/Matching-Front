c0f0843730e11689ae882c251590dcf26f335446
Leonardo Risco <leo99153@gmail.com>
2024-01-08 13:50:49 -0500

    estilos funcionAdmin terminados

--- src/components/crearClub/CrearClub.jsx
+++ src/components/crearClub/CrearClub.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import axios from 'axios'
+import styles from './CrearClub.module.css';
 
 const CrearClub = ({crearClub, setCrearClub}) =>{ 
     const dispatch = useDispatch();
@@ -30,43 +31,57 @@ const CrearClub = ({crearClub, setCrearClub}) =>{
     }
 
   return (
-    <>
-    <form onSubmit={handleSubmit(onSubmitClubs)}>
-            <div>
-                <label>Name:</label>
-                <input type="text" {...register('name', {required: true, maxLength: 20})}/>
+    <div className = {styles.holeCompContainer}>
+    <form onSubmit={handleSubmit(onSubmitClubs)} className = {styles.formContainer}>
+      <div className = {styles.inputContainer}>
+                
+                <input type="text" {...register('name', {required: true, maxLength: 20})} className = {styles.input}/>
+                <label className = {styles.label}>Name</label>
                 {errors.name?.type === "required" && <p>This field is required</p>}
                 {errors.name?.type === "maxLength" && <p>The max in the field is 20 characters</p>}
-                <label>Showers:</label>
-                <input type="text" {...register('showers', {required: true, maxLength: 20})}/>
+                </div>
+                <div className = {styles.inputContainer}>
+                
+                <input type="text" {...register('showers', {required: true, maxLength: 20})} className = {styles.input}/>
+                <label className = {styles.label}>Showers</label>
                 {errors.showers?.type === "required" && <p>This field is required</p>}
                 {errors.showers?.type === "maxLength" && <p>The max in the field is 20 characters</p>}
-                <label>Grills:</label>
-                <input type="text" {...register('grills', {required: true, maxLength: 20})}/>
+                </div>
+                <div className = {styles.inputContainer}>
+                
+                <input type="text" {...register('grills', {required: true, maxLength: 20})} className = {styles.input}/>
+                <label className = {styles.label}>Grills</label>
                 {errors.grills?.type === "required" && <p>This field is required</p>}
                 {errors.grills?.type === "maxLength" && <p>The max in the field is 20 characters</p>}
-                <label>Parking:</label>
-                <input type="text" {...register('parking', {required: true, maxLength: 20})}/>
+                </div>
+                <div className = {styles.inputContainer}>
+                
+                <input type="text" {...register('parking', {required: true, maxLength: 20})} className = {styles.input}/>
+                <label className = {styles.label}>Parking</label>
                 {errors.parking?.type === "required" && <p>This field is required</p>}
                 {errors.parking?.type === "maxLength" && <p>The max in the field is 20 characters</p>}
-                <label>Security:</label>
-                <input type="text" {...register('security', {required: true, maxLength: 20})}/>
+                </div>
+                <div className = {styles.inputContainer}>
+                
+                <input type="text" {...register('security', {required: true, maxLength: 20})} className = {styles.input}/>
+                <label className = {styles.label}>Security</label>
                 {errors.security?.type === "required" && <p>This field is required</p>}
                 {errors.security?.type === "maxLength" && <p>The max in the field is 20 characters</p>}
-
-                <label htmlFor="locationSelect">Select Location:</label>
-      <select id="locationSelect">
+                </div>
+                <div className = {styles.inputContainer}>
+      <select id="locationSelect" className = {styles.input}>
         {location?.map(location => (
           <option key={location.id} value={location.id}>
             {location.name}
           </option>
         ))}
       </select>
-            </div>
-         <button type="submit" value='enviar'> Create </button>
+      <label htmlFor="locationSelect" className = {styles.label}>Location</label>
+      </div>
+         <button type="submit" value='enviar' className = {styles.btnSubmit}> Create </button>
         </form>
-        <button onClick={()=>setCrearClub(false)}>cerrar</button>
-        </>
+        <button onClick={()=>setCrearClub(false)} className = {styles.close}>Cerrar</button>
+        </div>
   )
 }
 
