3cf9d5a91c52e0316379ded7499e74970ba3d551
diegogonr <diegog@explorandes.com>
2024-01-15 14:21:19 -0500

    update google login localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const theme = createTheme();
 
 // axios.defaults.baseURL = 'postgres://matching_db_wl28_user:f8XXqdrmkeMNj2jdt99wme0gYqBuWVCW@dpg-clud6t0l5elc73eh0cc0-a/matching_db_wl28';
-//  axios.defaults.baseURL = 'http://localhost:3000';
-axios.defaults.baseURL = 'https://matchingsv.onrender.com'
+ axios.defaults.baseURL = 'http://localhost:3000';
+// axios.defaults.baseURL = 'https://matchingsv.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
